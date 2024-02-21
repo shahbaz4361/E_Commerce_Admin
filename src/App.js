11d7be441101949d34c92a3ccd348fd9e004ec2f
Shahbaz Jamal <135946699+shahbaz4361@users.noreply.github.com>
2024-02-21 22:50:25 +0530

    First Commits

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@
 * Copyright 2023 Creative Tim (https://www.creative-tim.com)
 
 Coded by www.creative-tim.com
-
-// https://github.com/shahbaz4361/E_Commerce_Admin-----------------------------------
-
  =========================================================
 
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
